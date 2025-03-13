4c139b073559f12aaa51949c22e53b83aa2ffbd7
SefaliSabnam <sefali.sabnam26@gmail.com>
2025-03-13 13:02:25 +0530

    master one

--- lambda_function.py
+++ lambda_function.py
@@ -3,5 +3,5 @@ import json
 def lambda_handler(event, context):
     return {
         'statusCode': 200,
-        'body': json.dumps('Hello from Lambda! Deployed via Jenkins by Sefali!')
+        'body': json.dumps('Hello from Lambda! Deployed via Jenkins')
     }
